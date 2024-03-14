f7e00500e56c076bd978e93b9f1ec7891ba082ce
Andri450 <andrisetiawan010101@gmail.com>
2024-03-14 21:11:06 +0700

    push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,29 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var nama: TextView
-    private lateinit var email: TextView
-    private lateinit var alamat: TextView
-    private lateinit var hp: TextView
-    private lateinit var btnKirim: Button
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nama = findViewById(R.id.nama)
-        email = findViewById(R.id.email)
-        alamat = findViewById(R.id.alamat)
-        hp = findViewById(R.id.hp)
-        btnKirim = findViewById(R.id.button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        btnKirim.setOnClickListener{
-            val intentTujuan = Intent(this@MainActivity, MainActivity2::class.java);
-            intentTujuan.putExtra(MainActivity2.namadat, nama.text.toString())
-            startActivity(intentTujuan);
+        binding.button.setOnClickListener {
+            binding.textView.text = "Hello, ini pake Binding!"
         }
 
     }
